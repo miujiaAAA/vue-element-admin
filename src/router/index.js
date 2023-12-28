861f56e67e837d2c603347555a9c967e26889f47
miujiaAAA <1396871192@qq.com.com>
2023-12-28 14:47:28 +0800

    删除模拟数据

--- src/router/index.js
+++ src/router/index.js
@@ -171,19 +171,6 @@ export const asyncRoutes = [
     ]
   },
 
-  {
-    path: '/icon',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/icons/index'),
-        name: 'Icons',
-        meta: { title: 'icons', icon: 'icon', noCache: true }
-      }
-    ]
-  },
-
   /** when your routing map is too long, you can split it into small modules **/
   componentsRouter,
   chartsRouter,
